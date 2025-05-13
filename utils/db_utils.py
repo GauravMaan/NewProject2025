da603694ae4375a11c33bbbc0d561840cd3d5213
GauravMaan <gauravmaan0009@gmail.com>
2025-05-13 19:03:10 +0530

    Commit

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -5,7 +5,7 @@ def get_test_data_from_db():
     connection = pymysql.connect(
         host='localhost',
         user='root',
-        password='vivek@160723',
+        password='root@123',
         db='pytest_db'
     )
     cursor = connection.cursor()
